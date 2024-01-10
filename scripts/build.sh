3278ec1f9c4a7a3bd5a0bf2eef1414e85ca0ba93
zijiren233 <pyh1670605849@gmail.com>
2024-01-11 00:33:22 +0800

    Fix: del riscv32

--- scripts/build.sh
+++ scripts/build.sh
@@ -21,10 +21,11 @@ function Init() {
 function Help() {
     echo "-h: help"
     echo "-t: test build only"
+    echo "-T: targets file path or targets string"
 }
 
 function ParseArgs() {
-    while getopts "ht" arg; do
+    while getopts "htT:" arg; do
         case $arg in
         h)
             Help
@@ -32,6 +33,9 @@ function ParseArgs() {
             ;;
         t)
             TEST_BUILD_ONLY="1"
+            ;;
+        T)
+            TARGETS_FILE="$OPTARG"
             ;;
         ?)
             echo "unkonw argument"
@@ -43,7 +47,8 @@ function ParseArgs() {
 
 function Build() {
     TARGET="$1"
-    make TARGET=${TARGET} GCC_VER="11.4.0" \
+    make TARGET=${TARGET} \
+        GCC_VER="11.4.0" \
         MUSL_VER="1.2.4" \
         BINUTILS_VER="2.41" \
         GMP_VER="6.3.0" \
@@ -99,25 +104,35 @@ powerpc64-linux-musl
 powerpc64le-linux-musl
 powerpcle-linux-musl
 powerpcle-linux-muslsf
-riscv32-linux-musl
 riscv64-linux-musl
 s390x-linux-musl
 x86_64-linux-musl
 x86_64-linux-muslx32'
 
 function BuildAll() {
-    # while read line; do
-    #     if [ -z "$line" ] || [ "${line:0:1}" == "#" ]; then
-    #         continue
-    #     fi
-    #     Build "$line"
-    # done <scripts/triples.txt
+    if [ "$TARGETS_FILE" ]; then
+        if [ -f "$TARGETS_FILE" ]; then
+            while read line; do
+                if [ -z "$line" ] || [ "${line:0:1}" == "#" ]; then
+                    continue
+                fi
+                Build "$line"
+            done <"$TARGETS_FILE"
+            return
+        else
+            TARGETS="$TARGETS_FILE"
+        fi
+    else
+        TARGETS="$ALL_TARGETS"
+    fi
 
-    for line in $ALL_TARGETS; do
+    for line in $TARGETS; do
         if [ -z "$line" ] || [ "${line:0:1}" == "#" ]; then
             continue
         fi
-        Build "$line"
+        for target in ${line//,/ }; do
+            Build "$target"
+        done
     done
 }
 
