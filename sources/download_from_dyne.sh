335ad7809a820c2673cf9a5c6971a4a49c213252
Jaromil <jaromil@dyne.org>
2025-07-19 12:30:21 +0200

    fix: build all targets, trim build config and compute hashes
    upload to files.dyne.org

--- sources/download_from_dyne.sh
+++ sources/download_from_dyne.sh
@@ -5,7 +5,7 @@
 # scp all contents of sources subdir
 DYNE="https://files.dyne.org/?file=musl/sources"
 
-file="binutils-2.44.tar.xz"; echo "curl $file"; curl -L -o "$file" "${DYNE}/$file"
+file="binutils-2.44.tar.gz"; echo "curl $file"; curl -L -o "$file" "${DYNE}/$file"
 file="config.sub"; echo "curl $file"; curl -L -o "$file" "${DYNE}/$file"
 file="gcc-14.2.0.tar.xz"; echo "curl $file"; curl -L -o "$file" "${DYNE}/$file"
 file="gmp-6.1.2.tar.bz2"; echo "curl $file"; curl -L -o "$file" "${DYNE}/$file"
